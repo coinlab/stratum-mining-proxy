e6d8d27d9dded75587e8dd6085c540972693075e
Bobby Seidensticker <bseidensticker@gmail.com>
2013-11-08 16:06:28 -0800

    updated wowrack forever script, it works

---
+++ wowrack-btcguild.js
@@ -0,0 +1,13 @@
+var forever = require('forever-monitor');
+
+var port = 51;
+var command;
+for (var i = 0; i < 25; i++) {
+    command = "./mining_proxy.py -o 50.31.149.57 -p 3333 -sp 33" + port + " -oh 10.100.0.9 -gp 83" + port + " -nm -cu Alydian_w" + (port - 51);
+    forever.start(command.split(' '), {
+        'append': true,
+        'silent': true,
+        'errFile': "/home/ubuntu/src/stratum-mining-proxy/logs/w" + i + ".log"
+    });
+    port++;
+}
