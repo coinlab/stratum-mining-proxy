466b7b1e54480ea0ceaeee79a261bb4f5dd1b421
Bobby Seidensticker <bseidensticker@gmail.com>
2013-11-21 15:08:49 -0800

    wowrack-btcguild forever script

--- wowrack-btcguild.js
+++ wowrack-btcguild.js
@@ -2,8 +2,10 @@ var forever = require('forever-monitor');
 
 var port = 51;
 var command;
-for (var i = 0; i < 25; i++) {
-    command = "./mining_proxy.py -o 50.31.149.57 -p 3333 -sp 33" + port + " -oh 10.100.0.9 -gp 83" + port + " -nm -cu Alydian_w" + (port - 51);
+for (var i = 0; i < 85; i++) {
+    real_port = 8300 + port
+    real_stport = 3300 + port
+    command = "./mining_proxy.py -o 50.31.149.57 -p 3333 -sp " + real_stport + " -oh 10.100.0.9 -gp " + real_port + " -nm -cu Alydian_w" + (port - 51);
     forever.start(command.split(' '), {
         'append': true,
         'silent': true,
